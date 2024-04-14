3b31ac1f5f0bfc1fae0fbe1267179956b7e088f7
Patricio Bustamante <130309115+patob2000@users.noreply.github.com>
2024-04-14 16:15:31 -0400

    Create Inicio.py

---
+++ Inicio.py
@@ -0,0 +1,44 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Conceptos Técnicos Clave en IA",
+    page_icon="🤖"
+)
+
+
+st.markdown(
+    """
+    # Conceptos Técnicos Clave en IA
+"""
+)
+
+st.markdown("### 😊 Bienvenda")
+
+st.markdown("""
+
+Soy Patricio.  
+Apasionado de la inteligencia artificial, educación y marketing.
+
+Este libro te revela cinco claves esenciales.  
+Usaremos la tecnología para enriquecer tus libros digitales.
+
+Exploraremos herramientas avanzadas.  
+Podrás crear contenido educativo y promocional.  
+Todo gracias a la inteligencia artificial.
+
+Aprenderás rápidamente.  
+Todo de manera que cualuier persona lo entienda.
+
+Prepárate para una revolución.  
+Tecnología y creatividad unidas.  
+Transformaremos cómo construimos libros digitales.
+
+¡Comencemos este emocionante viaje juntos!
+""")
+
+
+container = st.container(border=True)
+container.page_link("pages/01_📑_Introducción.py", label="Click aquí para Continuar ...", icon="👉")
+            
+
+
