ee34517fe5b463d0dd4ba89593a0719aadd9a9c8
Patricio Bustamante <130309115+patob2000@users.noreply.github.com>
2024-04-14 21:27:41 -0400

    Create 07_📑_Autor.py

---
+++ pages/07_📑_Autor.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+
+
+st.markdown("""
+
+Soy Patricio.
+Apasionado de la inteligencia artificial, la educación y el marketing.
+
+Este libro te reveló claves esenciales.
+Ahora usarás la tecnología para enriquecer tus libros digitales.
+
+Podrás crear contenido educativo y promocional.
+Todo gracias a la inteligencia artificial.
+
+Este material fue creado para que aprendieras rápidamente.
+Todo de manera que cualquier persona lo entienda.
+
+Prepárate para una revolución.
+Tecnología y creatividad unidas.
+Transformaremos cómo construimos libros digitales.
+
+¡Este es solo el comienzo de este emocionante viaje!
+""")
+
+st.markdown("---")
+st.image('patricio_bustamante.png', caption='Patricio Bustamante' , width = 120)
+
+
+
